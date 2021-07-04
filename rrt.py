f3cfba061f6b2fc60ba24535ff6ee164f6aab998
George Krait <54797630+gkrait@users.noreply.github.com>
2021-07-04 17:19:41 +0200

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -10,6 +10,64 @@ from PIL import Image
 
 
 
+def myPathPlanning(grid, start,goal,d=4,anim=False,diric=""):
+	Flage=False
+	G_start = nx.Graph()
+	G_start.add_node(tuple(start))
+	parents_start={tuple(start):"None"}
+	G_goal = nx.Graph()
+	G_goal.add_node(tuple(goal))
+	parents_goal={tuple(goal):"None"}
+	if anim ==True: ind=0;
+	while not Flage:
+		new_point=randomCellGen(grid,start,goal)
+		Freeness_start=isFree(G_start,grid,new_point,d)
+		Freeness_goal=isFree(G_goal,grid,new_point,d)
+		if Freeness_start[0]:
+			G_start = nx.compose(Freeness_start[1],G_start)
+			parents_start.update(Freeness_start[2])
+		if Freeness_goal[0]:
+			G_goal = nx.compose(Freeness_goal[1],G_goal)
+			parents_goal.update(Freeness_goal[2])		
+		
+		if anim==True:
+			animate_plotting(deepcopy(grid),start,goal,list(G_start.nodes)+list(G_goal.nodes),ind,color="gray",diric=diric);ind +=1 #;plt.show() ;input("hi")
+		
+
+		if 	Freeness_start[0] and Freeness_goal[0] and Freeness_start[3]==tuple(new_point) and Freeness_goal[3]==tuple(new_point):
+			Flage=True
+
+	
+	# Computing the path
+	first_part=list(G_start.nodes)
+	second_part=list(G_goal.nodes)
+	Path1=[tuple(Freeness_start[3])]
+	pointer=Path1[0]
+	while True:
+		new_pointer=parents_start[tuple(pointer)]
+		Path1.append(new_pointer)
+		pointer=new_pointer
+		if pointer==tuple(start):
+			break
+	Path1.reverse()
+	Path2=[tuple(Freeness_goal[3])]
+	pointer=Path2[0]	
+	while True:
+		new_pointer=parents_goal[pointer]
+		Path2.append(new_pointer)
+		pointer=new_pointer
+		if pointer==tuple(goal):
+			break		
+	
+	grid_copy=grid[:]
+	if anim==True:
+		animate_plotting(deepcopy(grid),start,goal,Path1 +Path2,ind,color="blue",diric=diric);ind +=1 
+		algo_animation(grid, start,goal,Path1 +Path2,ind,diric=diric)
+		if ind > len(grid[0])*len(grid):
+			print("There is no path")
+			return [] 
+
+	return Path1 +Path2
 
 
 def closest_node(point, G):
@@ -116,66 +174,6 @@ def algo_animation(grid, start,goal,path,ind,diric=""):
 
     
 
-def myPathPlanning(grid, start,goal,d=4,anim=False,diric=""):
-	Flage=False
-	G_start = nx.Graph()
-	G_start.add_node(tuple(start))
-	parents_start={tuple(start):"None"}
-	G_goal = nx.Graph()
-	G_goal.add_node(tuple(goal))
-	parents_goal={tuple(goal):"None"}
-	if anim ==True: ind=0;
-	while not Flage:
-		new_point=randomCellGen(grid,start,goal)
-		Freeness_start=isFree(G_start,grid,new_point,d)
-		Freeness_goal=isFree(G_goal,grid,new_point,d)
-		if Freeness_start[0]:
-			G_start = nx.compose(Freeness_start[1],G_start)
-			parents_start.update(Freeness_start[2])
-		if Freeness_goal[0]:
-			G_goal = nx.compose(Freeness_goal[1],G_goal)
-			parents_goal.update(Freeness_goal[2])		
-		
-		if anim==True:
-			animate_plotting(deepcopy(grid),start,goal,list(G_start.nodes)+list(G_goal.nodes),ind,color="gray",diric=diric);ind +=1 #;plt.show() ;input("hi")
-		
-
-		if 	Freeness_start[0] and Freeness_goal[0] and Freeness_start[3]==tuple(new_point) and Freeness_goal[3]==tuple(new_point):
-			Flage=True
-
-	
-	# Computing the path
-	first_part=list(G_start.nodes)
-	second_part=list(G_goal.nodes)
-	Path1=[tuple(Freeness_start[3])]
-	pointer=Path1[0]
-	while True:
-		new_pointer=parents_start[tuple(pointer)]
-		Path1.append(new_pointer)
-		pointer=new_pointer
-		if pointer==tuple(start):
-			break
-	Path1.reverse()
-	Path2=[tuple(Freeness_goal[3])]
-	pointer=Path2[0]	
-	while True:
-		new_pointer=parents_goal[pointer]
-		Path2.append(new_pointer)
-		pointer=new_pointer
-		if pointer==tuple(goal):
-			break		
-	
-	grid_copy=grid[:]
-	if anim==True:
-		animate_plotting(deepcopy(grid),start,goal,Path1 +Path2,ind,color="blue",diric=diric);ind +=1 
-		algo_animation(grid, start,goal,Path1 +Path2,ind,diric=diric)
-		if ind > len(grid[0])*len(grid):
-			print("There is no path")
-			return [] 
-
-
-	
-	return Path1 +Path2
 
 
 
